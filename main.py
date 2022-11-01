140833174191fc7a7a9dcc2f9e53174cf27f5f73
irynagrape <109566237+irynagrape@users.noreply.github.com>
2022-11-01 19:27:31 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from calendar import*
+
+year = int(input('Enter Year: '))
+
+print(calendar(year, 2, 1, 8, 4))
+
+
+
+#2 = 2 characters for days (Mo,Tu, etc)
+
+#1 = 1 line (row) for each week
+
+#8 = 8 rows for each month
+
+#4 = 4 columns for all months of the year.
